0b217b28b9a6328f72c46d615edf2c24466516ea
jass0034 <jass00050034@gmail.com>
2026-09-07 16:58:33 +0530

    commit  Fix Name

--- src/services/login-api/pages.js
+++ src/services/login-api/pages.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
-// const API_URl = 'https://localhost:7156/api/Login';
+// const API_URL = 'https://localhost:7156/api/Login';
 const API_BASE_URL = (
   process.env.NEXT_PUBLIC_API_URL || 'https://rolebase-api-2.onrender.com'
 ).replace(/\/$/, '');
 const API_URL = `${API_BASE_URL}/api/Login`;
 
 export const sendOtp = (data) => {
-  return axios.post(`${API_URl}/sendOtp`, data);
+  return axios.post(`${API_URL}/sendOtp`, data);
 };
 
 export const verifyOtp = (data) => {
-  return axios.post(`${API_URl}/verifyOtp`, data);
+  return axios.post(`${API_URL}/verifyOtp`, data);
 };
